97e223e815cd65e428acc55297a0da4e23de07cc
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2021-10-20 17:40:06 -0700

    deeper learning

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const person = {
+  name: "Isaac",
+  walk() {
+    console.log(this)
+  }
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+person.walk();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const walk = person.walk.bind(person);
+walk();
