74e63f41cda449d76a04a99933a8dd112724c8a1
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2021-11-17 17:02:56 -0800

    data fetching

--- src/components/Fordata.js
+++ src/components/Fordata.js
@@ -1,32 +1,40 @@
 import React, { useState, useEffect } from 'react';
 
 
+
 const Fordata = () => {
-    const [users, setUsers] = useState("")
+    const [users, setUsers] = useState({})
+    // const [myData, setMyData] = useState({})
 
     useEffect(() => {
         fetchUsers();
-    }, [])
+    }, []);
 
-    const fetchUsers = async () => {
-        const response = await fetch("http://localhost:3000/data.json");
-        const data = await response.json();
-        try {
-            setUsers(data.results);
-        } catch (err) {
-            console.log(err)
-        }
-};
+   
+   const fetchUsers = async () => {
+       const response = await fetch("http://localhost:3000/data.json")
+       const data = await response.json();
+       console.log(data)
+       try {
+           setUsers(data.response);
+       } catch (errors) {
+           console.log(errors);
+       }
+   } ;
 
     return(
         <div>
-            {users && users.map(user => (
-                <li key={user.login.uuid}>
-                    {user.name.first} {user.name.last}
-                </li>
-            ))}
             <h1>Data here</h1>
+            <label htmlFor="data">Data:</label><br />
+            {!users && <textarea id="data" rows="12" cols="60" />}
+            {users && (
+                <textarea id="data" row="12" cols="60" readOnly values={JSON.stringify(users)} />
+            )}
+            <br />
+            <button onClick={fetchUsers}>Load Data</button>
         </div>
+
+        
     )
 }
 
